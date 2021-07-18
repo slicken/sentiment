d01a59368b458615298891df9cd049ce5cb6b0ab
slicken <slicken@gmail.com>
2021-07-18 20:20:08 +0200

    upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/slicken/sentiment
+
+go 1.14
---
+++ sentiment.go
@@ -0,0 +1,56 @@
+package sentiment
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type SentimentData struct {
+	Value     int
+	ValueName string
+	Timestamp time.Time
+}
+
+type Sentiment []SentimentData
+
+type sentimentBlob struct {
+	Name string `json:"name"`
+	Data []struct {
+		Value     string `json:"value"`
+		ValueName string `json:"value_classification"`
+		Timestamp string `json:"timestamp"`
+		TimeUntil string `json:"time_until_update"`
+	}
+	Metadata interface{} `json:"metadata"`
+}
+
+// GetSentiment returns list of SentimentData for N(=limit) days
+func GetSentiment(limit int) (Sentiment, error) {
+	resp, err := http.Get(fmt.Sprintf("https://api.alternative.me/fng/?limit=%d", limit))
+	if err != nil || resp.StatusCode != 200 {
+		return Sentiment{}, err
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Sentiment{}, err
+	}
+	resp.Body.Close()
+
+	var blob sentimentBlob
+	json.Unmarshal(data, &blob)
+
+	var list = make(Sentiment, len(blob.Data))
+	for i, v := range blob.Data {
+		val, _ := strconv.Atoi(v.Value)
+		ts, _ := strconv.Atoi(v.Timestamp)
+		list[i].Value = val
+		list[i].ValueName = v.ValueName
+		list[i].Timestamp = time.Unix(int64(ts), 0)
+	}
+
+	return list, nil
+}
